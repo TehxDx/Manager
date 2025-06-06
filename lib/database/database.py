c1cdad5fe6e5555e747f3345aa2d4eeaecf2cbf9
temptxx <developer@tricen.co>
2025-06-06 11:42:35 -0400

    adding in ticket support

--- lib/database/database.py
+++ lib/database/database.py
@@ -2,7 +2,7 @@
     I did not like the way lowDB worked, so I am redoing the database and moving to SQLite3
 """
 import sqlite3
-
+import logging
 class ManagerDB:
 
     def __init__(self):
@@ -27,15 +27,15 @@ class ManagerDB:
         # lets initialize the banned table
         ban_table = '''
                     CREATE TABLE IF NOT EXISTS banned (
-                                                          id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                                          guild_id INTEGER NOT NULL,
-                                                          discord_id INTEGER NOT NULL,
-                                                          discord_name TEXT NOT NULL,
-                                                          reason TEXT,
-                                                          banned_by INTEGER NOT NULL,
-                                                          timestamp INTEGER NOT NULL DEFAULT (strftime('%s', 'now')),
-                                                          UNIQUE (guild_id, discord_id)
-                        );
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        guild_id INTEGER NOT NULL,
+                        discord_id INTEGER NOT NULL,
+                        discord_name TEXT NOT NULL,
+                        reason TEXT,
+                        banned_by INTEGER NOT NULL,
+                        timestamp INTEGER NOT NULL DEFAULT (strftime('%s', 'now')),
+                        UNIQUE (guild_id, discord_id)
+                    );
                     '''
         ban_table_index = '''CREATE INDEX IF NOT EXISTS idx_banned_guild_user ON banned (guild_id, discord_id);'''
         ban_table_index_2 = '''CREATE INDEX IF NOT EXISTS idx_banned_user ON banned (discord_id);'''
@@ -46,13 +46,13 @@ class ManagerDB:
         # lets initialize the kick table
         kick_table = '''
                      CREATE TABLE IF NOT EXISTS kicks (
-                                                          id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                                          discord_id INTEGER NOT NULL,
-                                                          discord_name TEXT NOT NULL,
-                                                          guild_id INTEGER NOT NULL,
-                                                          reason TEXT,
-                                                          kicked_by INTEGER NOT NULL,
-                                                          timestamp INTEGER NOT NULL DEFAULT (strftime('%s', 'now'))
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        discord_id INTEGER NOT NULL,
+                        discord_name TEXT NOT NULL,
+                        guild_id INTEGER NOT NULL,
+                        reason TEXT,
+                        kicked_by INTEGER NOT NULL,
+                        timestamp INTEGER NOT NULL DEFAULT (strftime('%s', 'now'))
                      );
                      '''
         kick_table_index = '''CREATE INDEX IF NOT EXISTS idx_kicks_discord_id ON kicks (discord_id);'''
@@ -66,14 +66,14 @@ class ManagerDB:
         # lets initialize the timeout table
         timeout_table = '''
                         CREATE TABLE IF NOT EXISTS timeout (
-                                                                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                                                discord_id INTEGER NOT NULL,
-                                                                discord_name TEXT NOT NULL,
-                                                                guild_id INTEGER NOT NULL,
-                                                                reason TEXT,
-                                                                timeout_by INTEGER NOT NULL,
-                                                                timeout_until INTEGER NOT NULL,
-                                                                timestamp INTEGER NOT NULL DEFAULT (strftime('%s', 'now'))
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            discord_id INTEGER NOT NULL,
+                            discord_name TEXT NOT NULL,
+                            guild_id INTEGER NOT NULL,
+                            reason TEXT,
+                            timeout_by INTEGER NOT NULL,
+                            timeout_until INTEGER NOT NULL,
+                            timestamp INTEGER NOT NULL DEFAULT (strftime('%s', 'now'))
                         );
                         '''
         timeout_table_index = '''CREATE INDEX IF NOT EXISTS idx_timeouts_discord_id ON timeout (discord_id);'''
@@ -86,6 +86,40 @@ class ManagerDB:
         self._exec_query(timeout_table_index_3, commit=True)
         self._exec_query(timeout_table_index_4, commit=True)
 
+        ticket_setup_table = '''
+                             CREATE TABLE IF NOT EXISTS ticket_setup (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                guild_id INTEGER NOT NULL,
+                                discord_id INTEGER NOT NULL,
+                                message_id INTEGER NOT NULL,
+                                category INTEGER NOT NULL,
+                                channel INTEGER NOT NULL,
+                                roles TEXT,
+                                timestamp INTEGER NOT NULL DEFAULT (strftime('%s', 'now'))
+                             );
+                             '''
+        ticket_setup_index = '''CREATE INDEX IF NOT EXISTS idx_ticket_id ON ticket_setup (guild_id, discord_id);'''
+        ticket_setup_index2 = '''CREATE INDEX IF NOT EXISTS idx_ticket_message ON ticket_setup (message_id);'''
+        self._exec_query(ticket_setup_table, commit=True)
+        self._exec_query(ticket_setup_index, commit=True)
+
+        ticket_table = '''
+                        CREATE TABLE IF NOT EXISTS tickets (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            guild_id INTEGER NOT NULL,
+                            discord_id INTEGER NOT NULL,
+                            channel INTEGER NOT NULL,
+                            status BOOLEAN NOT NULL CHECK (status IN (0, 1)),
+                            timestamp INTEGER NOT NULL DEFAULT (strftime('%s', 'now'))
+                        );
+                       '''
+        ticket_index = '''CREATE INDEX IF NOT EXISTS idx_tickets_guild_id ON tickets (guild_id);'''
+        ticket_index_2 = '''CREATE INDEX IF NOT EXISTS idx_tickets_discord_id ON tickets (discord_id);'''
+        self._exec_query(ticket_table, commit=True)
+        self._exec_query(ticket_index, commit=True)
+        self._exec_query(ticket_index_2, commit=True)
+
+    # add a user to the banned table
     def add_user_banned(
             self,
             guild_id: int,
@@ -101,9 +135,10 @@ class ManagerDB:
                 ON CONFLICT (guild_id, discord_id)
                 DO NOTHING;
                 '''
-        test = self._exec_query(query, (guild_id, discord_id, discord_name, reason, banned_by, timestamp), commit=True)
-        print(test)
+        self._exec_query(query, (guild_id, discord_id, discord_name, reason, banned_by, timestamp), commit=True)
 
+    # add a user to the kicked table, though, if they are banned and exist in the table, it doesn't need to do anything
+    # though this will change, as unbans do happen, but at the moment its left as is
     def add_user_kicked(
             self,
             guild_id: int,
@@ -119,6 +154,7 @@ class ManagerDB:
                 '''
         self._exec_query(query, (discord_id, discord_name, guild_id, reason, kicked_by, timestamp), commit=True)
 
+    # add a user to the timeout table
     def add_user_timeout(
             self,
             discord_id: int,
@@ -135,6 +171,7 @@ class ManagerDB:
                 '''
         self._exec_query(query, (discord_id, discord_name, guild_id, reason, timeout_by, timeout_until, timestamp), commit=True)
 
+    # fetch the last 5 banned
     def fetch_ban_list(self, guild_id: int):
         query = '''
                 SELECT * FROM banned 
@@ -145,6 +182,7 @@ class ManagerDB:
         fetch = self._exec_query(query, (guild_id,), fetch_all=True)
         return fetch
 
+    # fetch the last 5 kicked
     def fetch_kick_list(self, guild_id: int):
         query = '''
                 SELECT * FROM kicks 
@@ -155,6 +193,7 @@ class ManagerDB:
         fetch = self._exec_query(query, (guild_id,), fetch_all=True)
         return fetch
 
+    # fetch the last 5 of the timeout list
     def fetch_timeout_list(self, guild_id: int):
         query = '''
                 SELECT * FROM timeout
@@ -164,3 +203,17 @@ class ManagerDB:
                 '''
         fetch = self._exec_query(query, (guild_id,), fetch_all=True)
         return fetch
+
+    # ticket database
+    # guild_id, discord_id, message_id, category, channel, roles, timestamp
+    def setup_ticket(self, guild_id: int, discord_id: int, message_id: int, category: int, channel: int, roles: str, timestamp: int):
+        query = '''
+                INSERT INTO ticket_setup(guild_id, discord_id, message_id, category, channel, roles, timestamp) 
+                VALUES (?, ?, ?, ?, ?, ?, ?);
+                '''
+        self._exec_query(query, (guild_id, discord_id, message_id, category, channel, roles, timestamp), commit=True)
+        query2 = '''
+                SELECT * FROM ticket_setup
+                WHERE message_id = ?
+                '''
+        return self._exec_query(query2, (message_id,), fetch_all=True)
