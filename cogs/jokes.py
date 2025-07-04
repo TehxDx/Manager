709c48583d3a5176971d12c98dd4fe89d2809e87
temptxx <developer@tricen.co>
2025-07-03 20:10:28 -0400

    Updated jokes.py to include additional notes and changed how jokes are shipped to the channel

--- cogs/jokes.py
+++ cogs/jokes.py
@@ -6,15 +6,20 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-import requests
-import logging
-import os
+import os, random, logging, requests
 
 class Jokes(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
         self.dad_joke_url= "https://icanhazdadjoke.com/"
+        """
+            JokeAPI Configuration, I've included Safe mode for friendly jokes, however, removing ?safe-mode will
+                include all types of nsfw jokes.
+                
+            Additionally, it is available in other languages, please check their API documentation for specific
+                configurations. 
+        """
         self.joke_api_url = "https://v2.jokeapi.dev/joke/Any?safe-mode" # only provides safe jokes and filters out bad ones.
         self.headers = {
             "User-Agent": "ManagerBot/1.0",
@@ -24,34 +29,45 @@ class Jokes(commands.Cog):
     @app_commands.command(name="dadjoke", description="Get a random dad joke!")
     async def dadjoke(self, interaction: discord.Interaction):
         logging.info(f"[DadJoke] {interaction.user.name} has requested a dad joke!")
-
+        # fetch the dadjoke api
         fetch = requests.get(self.dad_joke_url, headers=self.headers)
         if fetch.status_code != 200:
+            # if status code is anything other than success, reply with a basic error message and log it
             logging.error(f"[DadJoke] Could not fetch a dad joke. {fetch.status_code}")
+            # send error response to the user
             return await interaction.response.send_message("Could not fetch a dadjoke. Please try again later.")
         else:
+            # format the json
             response = fetch.json()
-            return await interaction.response.send_message(response['joke'])
+            # make the response in an embed
+            embed = discord.Embed(
+                color=discord.Color.dark_magenta(),
+            )
+            embed.description = response["joke"]
+            # ship the joke inside the embed to the user
+            return await interaction.response.send_message(embed=embed)
 
     @app_commands.command(name="joke", description="Get a random joke!")
     async def joke(self, interaction: discord.Interaction):
         fetch = requests.get(self.joke_api_url, headers=self.headers)
         if fetch.status_code != 200:
+            # if status code is anything other than success, reply with a basic error message and log it
             logging.error(f"[Joke] Could not fetch a joke. {fetch.status_code}")
+            # send error response to the user
             return await interaction.response.send_message("Could not fetch a joke. Please try again later.")
         else:
+            # format the json
             response = fetch.json()
+            # make the response in an embed
             embed = discord.Embed(
-                title="I got a joke for you!",
                 color=discord.Color.dark_magenta(),
-                description="_Powered by: Jokeapi.dev_"
             )
             if response['type'] == "single":
-                print(response)
-                embed.add_field(name=response['joke'], value="\u200b", inline=False)
+                # if single (one-liner jokes), just add into description
+                embed.description = response['joke']
             else:
-                embed.add_field(name=response['setup'], value="\u200b", inline=False)
-                embed.add_field(name="\u200b", value=f"||{response['delivery']}||", inline=False)
+                # if two part (setup and delivery), show setup, then hide using || for the answer
+                embed.description = f"{response['setup']}\n|| {response['delivery']} ||"
             embed.set_footer(text=f"Category: {response['category']}")
             return await interaction.response.send_message(embed=embed)
 
